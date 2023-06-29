5d06b6629157d35dd2456f7053587bd692465a97
Jantoom <75770311+Jantoom@users.noreply.github.com>
2023-06-29 10:17:16 +0800

    Added model enums, proxy now redirects endpoints that don't match any of the patterns to one of the health endpoints.

--- backend/src/core/models/violation.py
+++ backend/src/core/models/violation.py
@@ -1,26 +1,32 @@
-import uuid
-from datetime import datetime
+import uuid, enum, datetime
 from core.services import db
+
+class TypeEnum(enum.Enum):
+    NONE = 'NONE'
+    TWO_SPACES = 'TWO_SPACES'
+    
+class StatusEnum(enum.Enum):
+    NONE = 'NONE'
+    INCOMPLETE = 'INCOMPLETE'
+    COMPLETE = 'COMPLETE'
+    UPLOADED = 'UPLOADED'
+    CLASSIFYING = 'CLASSIFYING'
+    CLASSIFIED = 'CLASSIFIED'
+    PROCESSING = 'PROCESSING'
+    APPROVED = 'APPROVED'
+    REJECTED = 'REJECTED'
+    ERROR = 'ERROR'
 
 class Violation(db.Model):
     __tablename__ = 'violations'
 
     id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     user_id = db.Column(db.String(36), db.ForeignKey('users.id'), nullable=False)
-    created_at = db.Column(db.DateTime(), default=datetime.utcnow)
+    created_at = db.Column(db.DateTime(), default=datetime.datetime.utcnow)
+    input_type = db.Column(db.String(30), nullable=False, default=TypeEnum.NONE.value)
+    predicted_type = db.Column(db.String(30), nullable=False, default=TypeEnum.NONE.value)
+    status = db.Column(db.String(30), nullable=False, default=StatusEnum.NONE.value)
     resource_url = db.Column(db.String(200), nullable=False)
-    type = db.Column(db.String(30), default=None)
-    status = db.Column(db.String(15), nullable=False, default='UPLOADED')
-
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'user_id': self.user_id,
-            'created_at': self.created_at,
-            'resource_url': self.resource_url,
-            'type': self.type,
-            'status': self.status
-        }
     
     def __repr__(self):
-        return f'<Asset({self.id}, {self.user_id}, {self.created_at}, {self.resource_url}, {self.type}, {self.status})>'
+        return f'<Violation({self.id}, {self.user_id}, {self.created_at}, {self.input_type}, {self.predicted_type}, {self.status}, {self.resource_url})>'
